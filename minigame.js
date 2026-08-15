e23e90d096a18d1ad16361e78c4ce7d3b1505bd8
hoangtungv80 <hoangtungv80@gmail.com>
2026-08-15 15:39:03 +0700

    Add candidate: Ha Nguyen Ford Everest DNT-34

--- minigame.js
+++ minigame.js
@@ -101,6 +101,7 @@ document.addEventListener('DOMContentLoaded', () => {
         { id: 'car-31', name: 'Trương Tuấn Anh', model: 'BMW G20 2020', plate: 'DNT-31', team: 'DONATEAM', color: '#00d2d3', img: 'images/donagala.jpg', desc: 'BMW G20 2020 phong cách', votes: {} },
         { id: 'car-32', name: 'Lê Anh Tuấn', model: 'Honda Civic Gen8', plate: 'DNT-32', team: 'DONATEAM', color: '#e17055', img: 'images/venue.jpg', desc: 'Honda Civic Gen8 cá tính', votes: {} },
         { id: 'car-33', name: 'Linh Vũ', model: 'Honda City GM6', plate: 'DNT-33', team: 'DONATEAM', color: '#a29bfe', img: 'images/donafest.jpg', desc: 'Honda City GM6 năng động', votes: {} },
+        { id: 'car-34', name: 'Hà Nguyễn', model: 'Ford Everest', plate: 'DNT-34', team: 'DONATEAM', color: '#dfe6e9', img: 'images/donatrip.jpg', desc: 'Ford Everest mạnh mẽ', votes: {} },
         { id: 'car-00', name: 'Trần Ngọc Hiếu', model: 'Vinfast MPV 7', plate: 'DNT-00', team: 'DONATEAM', color: '#10ac84', img: 'images/donafest.jpg', desc: 'Vinfast MPV 7 ấn tượng', votes: {} }
     ];
     const OFFICIAL_25_CARS = OFFICIAL_30_CARS;
